4678260c24966e419c245185324b55038851abe2
louisebishop1 <louisebishop@Louises-MacBook-Pro-2.local>
2017-02-12 15:19:52 +0800

    Removed pure-nav

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,14 +2,16 @@
 $(document).ready(function() {
   // // Sticky map
     // content, start fix & end fix
-    var content = $("#map");
-    var startFix = $("#map");
-    var endFix = $("footer");
+      var content = $("#map");
+      var startFix = $("#map");
+      var endFix = $("footer");
+      var footerHeight = $("footer").height();
+      var headerHeight = $("header").height();
+      var heightThreshold = $(startFix).offset().top - headerHeight;
+      var heightThreshold_end = $(endFix).offset().top - footerHeight;
+      console.log("This is the end" + heightThreshold_end);
 
-    // Calculate how far from the top the element is
-    var heightThreshold = $(startFix).offset().top;
-    var heightThreshold_end  = $(endFix).offset().top;
-    console.log(heightThreshold);
+
     // Use if you want the element to only be fixed while it's in view
     // var heightThreshold_end  = $(startFix).offset().top +$(startFix).height() ;
 
@@ -17,11 +19,12 @@ $(document).ready(function() {
         var pixelsFromTop = $(document).scrollTop();
         console.log(pixelsFromTop);
         if (pixelsFromTop >= heightThreshold && pixelsFromTop <=  heightThreshold_end ) {
-            content.addClass(".bg-map-fixed");
+            content.addClass("bg-map-fixed");
+        } else if ((pixelsFromTop - 100) === heightThreshold_end) {
+            $("bg-map-fixed").css("top", heightThreshold_end);
         } else {
-            content.removeClass(".bg-map-fixed");
+            content.removeClass("bg-map-fixed");
         }
     });
-
 
 });
